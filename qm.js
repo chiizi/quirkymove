74cb50fcfe7e6e79d4e21d4a79b37c3e9ede3023
Hans Felde <m33pfish@gmail.com>
2016-02-09 23:20:44 -0600

    Finish vectors

--- qm.js
+++ qm.js
@@ -14,14 +14,26 @@ var Vector = (function() {
     set y(n) {
       return this.yl = n;
     }
-    get x() {
-      return this.xl;
+    get y() {
+      return this.yl;
+    }
+    set mag(n) {
+      var dir = this.dir;
+      this.x = n * Math.cos(dir);
+      this.y = n * Math.sin(dir);
+      return this.mag;
     }
     get mag() {
-      
+      return Math.sqrt(this.x * this.x + this.y * this.y);
     }
     set dir(turn) {
-      // var angle = 
+      var mag = this.mag;
+      this.x = mag * Math.cos(turn);
+      this.y = mag * Math.sin(turn);
+      return this.dir;
+    }
+    get dir() {
+      return Math.atan(this.y / this.x);
     }
   }
 })();
