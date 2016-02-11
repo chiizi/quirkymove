1e35dc56291c20a9de6632b4ab0a6533ff439aa6
Hans Felde <m33pfish@gmail.com>
2016-02-10 18:27:28 -0600

    Update qm.js

--- qm.js
+++ qm.js
@@ -1,39 +1,43 @@
-var Vector = (function() {
-  "use strict";
-  return class {
-    constructor(args) {
-      this.xl = args.x;
-      this.yl = args.y;
-    }
-    set x(n) {
-      return this.xl = n;
-    }
-    get x() {
-      return this.xl;
-    }
-    set y(n) {
-      return this.yl = n;
-    }
-    get y() {
-      return this.yl;
-    }
-    set mag(n) {
-      var dir = this.dir;
-      this.x = n * Math.cos(dir);
-      this.y = n * Math.sin(dir);
-      return this.mag;
-    }
-    get mag() {
-      return Math.sqrt(this.x * this.x + this.y * this.y);
-    }
-    set dir(turn) {
-      var mag = this.mag;
-      this.x = mag * Math.cos(turn);
-      this.y = mag * Math.sin(turn);
-      return this.dir;
-    }
-    get dir() {
-      return Math.atan(this.y / this.x);
-    }
+"use strict";
+var Vector = class {
+  constructor(args) {
+    this.xl = args.x;
+    this.yl = args.y;
   }
-})();
+  set x(n) {
+    return this.xl = n;
+  }
+  get x() {
+    return this.xl;
+  }
+  set y(n) {
+    return this.yl = n;
+  }
+  get y() {
+    return this.yl;
+  }
+  set mag(n) {
+    var dir = this.dir;
+    this.x = n * Math.cos(dir);
+    this.y = n * Math.sin(dir);
+    return this.mag;
+  }
+  get mag() {
+    return Math.sqrt(this.x * this.x + this.y * this.y);
+  }
+  set dir(turn) {
+    var mag = this.mag;
+    this.x = mag * Math.cos(turn);
+    this.y = mag * Math.sin(turn);
+    return this.dir;
+  }
+  get dir() {
+    return Math.atan(this.y / this.x);
+  }
+};
+
+var Thing = class {
+  constructor(args) {
+    
+  }
+};
