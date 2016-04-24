775484d8c5e098a366e76dd9e9fdac4bc839315b
Hans Felde <m33pfish@gmail.com>
2016-04-23 22:54:42 -0500

    Fix my stuff

--- qm-src.x.js
+++ qm-src.x.js
@@ -1,49 +1,29 @@
-"use strict";
-var Vector = class {
-  constructor(args) {
-    @xl = args.x;
-    @yl = args.y;
-  }
-  set x(n) {
-    return @xl = n;
-  }
-  get x() {
-    return @xl;
-  }
-  set y(n) {
-    return @yl = n;
-  }
-  get y() {
-    return @yl;
-  }
+"use strict"
+const vector = (x, y) => ({
+  x, y,
   set mag(n) {
-    var dir = @dir;
-    @x = n * Math.cos(dir);
-    @y = n * Math.sin(dir);
-    return @mag;
-  }
+    var dir = @dir
+    @x = n * Math.cos(dir)
+    @y = n * Math.sin(dir)
+    return @mag
+  },
   get mag() {
-    return Math.sqrt(@x * @x + @y * @y);
-  }
+    return Math.sqrt(@x * @x + @y * @y)
+  },
   set dir(turn) {
-    var mag = @mag;
-    @x = mag * Math.cos(turn);
-    @y = mag * Math.sin(turn);
-    return @dir;
+    var mag = @mag
+    @x = mag * Math.cos(turn)
+    @y = mag * Math.sin(turn)
+    return @
   }
   get dir() {
-    return Math.atan(@y / @x);
+    return Math.atan(@y / @x)
   }
-};
+}
 
-var Thing = class {
-  constructor(args) {
-    @x = args.x || 0;
-    @y = args.y || 0;
-    @z = args.z || 0;
-    @stepsPerSecond = args.stepsPerSecond || 1;
+const thing = (x, y, z, sps) => {
+  x, y, z, sps,
+  step(n = 1) {
+    
   }
-  step(n) {
-    n ||= 1;
-  }
-};
+})
